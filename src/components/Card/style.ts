5b52104f6ace54f105ee2b0a68c262389127fe05
sarival <sarival@sarival.com.br>
2020-07-31 06:34:55 -0300

    ajuste css card

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.header`
+export const Container = styled.div`
   background-color: #fff;
   color: #3a3a3a;
   border-radius: 5px;
