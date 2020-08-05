a20de48cff17ac1178cc3dfccbd9854a930356f7
sarival <sarival@sarival.com.br>
2020-08-05 18:24:18 -0300

    teste com signalr no menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,15 +1,53 @@
-import React from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useState, useEffect } from 'react';
+import * as signalR from '@microsoft/signalr';
 
 import { Header } from './style';
 
+const connection: signalR.HubConnection = new signalR.HubConnectionBuilder()
+  .withUrl('http://localhost:5000/trellurahub')
+  // .withAutomaticReconnect([
+  //   0,
+  //   3000,
+  //   3000,
+  //   3000,
+  //   3000,
+  //   3000,
+  //   3000,
+  //   3000,
+  //   3000,
+  //   3000,
+  //   3000,
+  //   3000,
+  //   5000,
+  //   10000,
+  //   15000,
+  //   30000,
+  // ])
+  .build();
+
 const Menu: React.FC = () => {
+  const [users, setUsers] = useState(0);
+
+  useEffect(() => {
+    connection.start().then(() => {
+      console.log(`[App] connectionId = ${connection.connectionId}`);
+    });
+  }, []);
+
+  connection.on('atualizarTotalUsuarios', (totalUser: number) =>
+    setUsers(totalUser),
+  );
+
   return (
     <Header>
       <h1>
         <a href="/">Trellura</a>
       </h1>
 
-      <div>2 pessoas</div>
+      <div>
+        <span>{users} pessoas</span>
+      </div>
     </Header>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
 
 import { Form, Mensagem } from './style';
 import Menu from '../../components/Menu';
